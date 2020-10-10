1969175d70847582c35a7824918d2d0b3d90bb37
Nasirul Haque Ridoy <dpridoy@gmail.com>
2020-10-10 20:30:57 +0600

    third commit - Populate RecyclerView

--- app/src/main/java/com/dpridoy/tvseries/activities/MainActivity.java
+++ app/src/main/java/com/dpridoy/tvseries/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.dpridoy.tvseries.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -9,26 +10,49 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.dpridoy.tvseries.R;
+import com.dpridoy.tvseries.adapter.TVShowsAdapter;
+import com.dpridoy.tvseries.databinding.ActivityMainBinding;
+import com.dpridoy.tvseries.models.TVShows;
 import com.dpridoy.tvseries.responses.TVShowsResponses;
 import com.dpridoy.tvseries.viewmodels.MostPopularTVShowsViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private MostPopularTVShowsViewModel viewModel;
+    private ActivityMainBinding activityMainBinding;
+    private List<TVShows> tvShows=new ArrayList<>();
+    private TVShowsAdapter tvShowsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        doInitialization();
+    }
+
+    private void doInitialization(){
+        activityMainBinding.tvShowsRecyclerView.setHasFixedSize(true);
         viewModel=new ViewModelProvider(this).get(MostPopularTVShowsViewModel.class);
+        tvShowsAdapter=new TVShowsAdapter(tvShows);
+        activityMainBinding.tvShowsRecyclerView.setAdapter(tvShowsAdapter);
         getMostPopularTVShows();
     }
 
     private void getMostPopularTVShows(){
+        activityMainBinding.setIsLoading(true);
         viewModel.getMostPopularTVShows(0).observe(this, new Observer<TVShowsResponses>() {
             @Override
             public void onChanged(TVShowsResponses tvShowsResponses) {
-                Toast.makeText(getApplicationContext(),"Total Pages: "+tvShowsResponses.getPages(),Toast.LENGTH_SHORT).show();
+                activityMainBinding.setIsLoading(false);
+                if (tvShowsResponses != null){
+                    if (tvShowsResponses.getTvShows()!=null){
+                        tvShows.addAll(tvShowsResponses.getTvShows());
+                        tvShowsAdapter.notifyDataSetChanged();
+                    }
+                }
                 Log.e("Pages: ",tvShowsResponses.getPages().toString());
             }
         });
---
+++ app/src/main/java/com/dpridoy/tvseries/adapter/TVShowsAdapter.java
@@ -0,0 +1,62 @@
+package com.dpridoy.tvseries.adapter;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.dpridoy.tvseries.R;
+import com.dpridoy.tvseries.databinding.ItemTvShowBinding;
+import com.dpridoy.tvseries.models.TVShows;
+
+import java.util.List;
+
+public class TVShowsAdapter extends RecyclerView.Adapter<TVShowsAdapter.TVShowViewHolder>{
+
+    private List<TVShows> tvShows;
+    private LayoutInflater layoutInflater;
+
+    public TVShowsAdapter(List<TVShows> tvShows) {
+        this.tvShows = tvShows;
+    }
+
+    @NonNull
+    @Override
+    public TVShowViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (layoutInflater==null){
+            layoutInflater=LayoutInflater.from(parent.getContext());
+        }
+        ItemTvShowBinding tvShowBinding= DataBindingUtil.inflate(
+                layoutInflater, R.layout.item_tv_show,parent,false
+        );
+        return new TVShowViewHolder(tvShowBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TVShowViewHolder holder, int position) {
+        holder.bindTVShow(tvShows.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return tvShows.size();
+    }
+
+    static class TVShowViewHolder extends RecyclerView.ViewHolder{
+
+        private ItemTvShowBinding itemTvShowBinding;
+
+        public TVShowViewHolder(ItemTvShowBinding itemTvShowBinding){
+            super(itemTvShowBinding.getRoot());
+            this.itemTvShowBinding=itemTvShowBinding;
+        }
+
+        public void bindTVShow(TVShows tvShows){
+            itemTvShowBinding.setTvShow(tvShows);
+            itemTvShowBinding.executePendingBindings();
+        }
+    }
+
+}
